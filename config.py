77263ff288fdd263595437214d63c3fd08ecb806
Martin Chorley <me@martinjc.com>
2017-10-31 21:56:04 +0000

    add mail details

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import os
 
-REPORT_TO = ["ChorleyMJ@cardiff.ac.uk"]
+from _credentials import uname, pwd
+
+REPORT_TO = ["martin.chorley@gmail.com"]
 INPUT_DIR = os.path.join(os.getcwd(), 'input')
 
 MODULE_TO_MODULE_LEADER_FILE = 'module_list.csv'
 MODULE_LEADER_TO_EMAIL_FILE = 'mllist2email.csv'
 ASSESSMENT_DETAILS_FILE = 'assessment.xlsx'
+
+SMTP_SERVER = "outlook.office365.com"
+SMTP_PORT = 587
+SMTP_USERNAME = uname
+SMTP_PASSWORD = pwd
